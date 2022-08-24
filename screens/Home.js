ee9467235ffb4603603a6edcc4fa5524a290355e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-08-24 23:16:18 +0530

    [FEAT]: Pull to refresh

--- screens/Home.js
+++ screens/Home.js
@@ -1,54 +1,44 @@
-import React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+
+import { FlatList, StyleSheet, RefreshControl } from "react-native";
 
 import PalettePreview from "../components/PalettePreview";
 
-const SOLARIZED = [
-  { colorName: "Base03", hexCode: "#002b36" },
-  { colorName: "Base02", hexCode: "#073642" },
-  { colorName: "Base01", hexCode: "#586e75" },
-  { colorName: "Base00", hexCode: "#657b83" },
-  { colorName: "Base0", hexCode: "#839496" },
-  { colorName: "Base1", hexCode: "#93a1a1" },
-  { colorName: "Base2", hexCode: "#eee8d5" },
-  { colorName: "Base3", hexCode: "#fdf6e3" },
-  { colorName: "Yellow", hexCode: "#b58900" },
-  { colorName: "Orange", hexCode: "#cb4b16" },
-  { colorName: "Red", hexCode: "#dc322f" },
-  { colorName: "Magenta", hexCode: "#d33682" },
-  { colorName: "Violet", hexCode: "#6c71c4" },
-  { colorName: "Blue", hexCode: "#268bd2" },
-  { colorName: "Cyan", hexCode: "#2aa198" },
-  { colorName: "Green", hexCode: "#859900" },
-];
-
-const RAINBOW = [
-  { colorName: "Red", hexCode: "#FF0000" },
-  { colorName: "Orange", hexCode: "#FF7F00" },
-  { colorName: "Yellow", hexCode: "#FFFF00" },
-  { colorName: "Green", hexCode: "#00FF00" },
-  { colorName: "Violet", hexCode: "#8B00FF" },
-];
-
-const FRONTEND_MASTERS = [
-  { colorName: "Red", hexCode: "#c02d28" },
-  { colorName: "Black", hexCode: "#3e3e3e" },
-  { colorName: "Grey", hexCode: "#8a8a8a" },
-  { colorName: "White", hexCode: "#ffffff" },
-  { colorName: "Orange", hexCode: "#e66225" },
-];
-
-const COLOR_PALETTES = [
-  { paletteName: "Solarized", colors: SOLARIZED },
-  { paletteName: "Frontend Masters", colors: FRONTEND_MASTERS },
-  { paletteName: "Rainbow", colors: RAINBOW },
-];
+const URL = "https://color-palette-api.kadikraman.now.sh/palettes";
 
 const Home = ({ navigation }) => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [colorPalette, setColorPalette] = useState();
+
+  const handleFetchColorPallets = useCallback(async () => {
+    const result = await fetch(URL);
+
+    if (result.ok) {
+      const pallettes = await result.json();
+
+      setColorPalette(pallettes);
+    }
+  }, []);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    await handleFetchColorPallets();
+    setIsRefreshing(false);
+  });
+
+  setTimeout(() => {
+    setIsRefreshing(false);
+  }, 1000);
+
+  useEffect(() => {
+    handleFetchColorPallets();
+  }, [handleFetchColorPallets]);
+
   return (
     <FlatList
       style={styles.list}
-      data={COLOR_PALETTES}
+      data={colorPalette}
       keyExtractor={(item) => item.paletteName}
       renderItem={({ item }) => (
         <PalettePreview
@@ -56,6 +46,9 @@ const Home = ({ navigation }) => {
           palette={item}
         />
       )}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
+      }
     />
   );
 };
