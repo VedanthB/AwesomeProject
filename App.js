53c10d06def4d8984f45ced7591e56b85dd65244
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-08-24 16:16:09 +0530

    [FEAT]: Components

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import ColorBox from "./components/ColorBox";
 
 const App = () => {
   return (
@@ -8,12 +9,13 @@ const App = () => {
         <Text style={styles.heading}>
           Here are some boxes of different colours
         </Text>
-        <View style={[styles.box, styles.cyan]}>
-          <Text style={styles.text}>Cyan #2aa198</Text>
-        </View>
-        <View style={[styles.box, styles.blue]}>
-          <Text style={styles.text}>Blue #268bd2</Text>
-        </View>
+        <ColorBox hexCode="#2aa198" colorName="Cyan" />
+
+        <ColorBox hexCode="#268bd2" colorName="Blue" />
+
+        <ColorBox hexCode="#d33682" colorName="Magenta" />
+
+        <ColorBox hexCode="#cb4b16" colorName="Orange" />
       </View>
     </SafeAreaView>
   );
---
+++ components/ColorBox.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ColorBox = (props) => {
+  const colorStyle = {
+    backgroundColor: props.hexCode,
+  };
+
+  return (
+    <View style={[styles.box, colorStyle]}>
+      <Text style={styles.text}>
+        {props.colorName} {props.hexCode}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: "bold",
+    color: "white",
+  },
+});
+
+export default ColorBox;
